ebbc7591072a7a59aeac662cf58de9fb6a9ea1f0
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-21 14:41:52 -0600

    updated

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums( id INT UNSIGNED AUTO_INCREMENT,
                      artist VARCHAR(50),
                      name VARCHAR(255),
-                     release_date CHAR(4),
+                    release_date CHAR(4),
                      sales DECIMAL(2),
                     PRIMARY KEY (id) );
 
---
+++ indexes.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+DESCRIBE departments;
+
+DESCRIBE dept_emp;
+
+DESCRIBE dept_manager;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE titles;
+
+
+
+
+
+
