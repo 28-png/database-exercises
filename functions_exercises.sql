cf409ef2717c77fbc75ca944c6854b9530071a69
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-21 11:10:33 -0600

    updated

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,17 +8,17 @@ WHERE (first_name = 'Irena'
   AND gender = 'M'
 ORDER BY last_name, first_name;
 
-SELECT CONCAT(first_name, last_name)
+SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
 WHERE last_name LIKE 'E%E'
 ORDER BY emp_no desc;
 
-SELECT CONCAT(first_name, last_name)
+SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
 WHERE first_name LIKE 'E%E'
 ORDER BY emp_no desc;
 
-SELECT DATEDIFF( hire_date, curdate())  FROM employees WHERE hire_date between '1990-1-1' and '1999-12-31'
+SELECT first_name, last_name, DATEDIFF(curdate(), hire_date)  FROM employees WHERE hire_date between '1990-1-1' and '1999-12-31'
 AND birth_date LIKE '%-12-25'
 ORDER BY birth_date, hire_date desc
 LIMIT 5 OFFSET 45;
