08706d0dbce12a46602832c6b2b9bfbf08396676
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-24 07:22:33 -0600

    updated

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,7 +2,10 @@ USE employees;
 
 SELECT first_name, last_name
 FROM employees
-WHERE emp_no = '101010' IN (
-    SELECT hire_date
+WHERE emp_no IN (
+   SELECT emp_no
     FROM employees
+    WHERE hire_date = '1990-10-22'
+
 )
+
