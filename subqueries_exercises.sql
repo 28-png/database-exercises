9a3e580adfc45fccc753e1cc25e6bf808dd9546e
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-24 07:30:23 -0600

    updated

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,5 +7,16 @@ WHERE emp_no IN (
     FROM employees
     WHERE hire_date = '1990-10-22'
 
+);
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+   SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+
 )
+
+
 
