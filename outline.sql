12735f61b475a500f7b91ed70fa2ee5ac8e3161a
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-24 10:13:30 -0600

    updated

--- outline.sql
+++ outline.sql
@@ -6,3 +6,28 @@ Description VARCHAR(255),
 PRIMARY KEY (id)
 )
 
+INSERT INTO ads(Categories) values
+('Help Wanted'),
+('Giveaway'),
+('furniture'),
+('Vehicle')
+
+CREATE TABLE users (
+id INT UNSIGNED NOT NULL AUTO-INCREMENT,
+names VARCHAR(255),
+emails VARCHAR(255),
+PRIMARY KEY (id)
+)
+
+INSERT INTO users(names, emails)
+values ('Matt Murphy', 'murphy.d.matthew28@gmail.com'),
+        ('Jacob Morris', 'morris.s.jacob44@gmail.com')
+
+
+
+
+
+
+
+
+
