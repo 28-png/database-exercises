01cbe620d53e652f53db0c0611aa3d11be01fc81
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-24 11:52:02 -0600

    updated

--- outline.sql
+++ outline.sql
@@ -1,34 +1,42 @@
+CREATE DATABASE IF NOT EXISTS addList;
+
+USE addList;
+
 CREATE TABLE ads (
-id INT UNSIGNED NOT NULL AUTO-INCREMENT,
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 Categories VARCHAR(255),
 Title VARCHAR(255),
 Description VARCHAR(255),
 PRIMARY KEY (id)
-)
+);
 
 INSERT INTO ads(Categories) values
 ('Help Wanted'),
 ('Giveaway'),
 ('furniture'),
-('Vehicle')
+('Vehicle');
 
 CREATE TABLE users (
-id INT UNSIGNED NOT NULL AUTO-INCREMENT,
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 names VARCHAR(255),
 emails VARCHAR(255),
 PRIMARY KEY (id)
-)
+);
 
 INSERT INTO users(names, emails)
 values ('Matt Murphy', 'murphy.d.matthew28@gmail.com'),
-        ('Jacob Morris', 'morris.s.jacob44@gmail.com')
+        ('Jacob Morris', 'morris.s.jacob44@gmail.com');
 
 CREATE TABLE users_ads (
     users_id INTEGER UNSIGNED NOT NULL,
     ads_id INTEGER UNSIGNED NOT NULL,
     FOREIGN KEY (users_id) REFERENCES users(id),
     FOREIGN KEY (ads_id) REFERENCES ads(id)
-)
+);
+
+
+
+
 
 
 
